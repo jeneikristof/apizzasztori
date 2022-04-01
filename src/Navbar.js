68d3b13086eea9b3eb3b7dac26bef109238f288f
jeneikristof <kjenei13@gmail.com>
2022-04-01 15:07:13 +0200

    navbar alakul

---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import nav from "./CSS/Navbar.module.css"
+import pizzasztori from "./images/pizzasztori.svg"
+import {BrowserRouter, Routes,Route,NavLink} from "react-router-dom"
+import Food from "./Food.js"
+export default function Navbar(){
+    return ( 
+    <>
+        <BrowserRouter >
+        <div className={nav.container}>
+            <div className={nav.left}>
+            <NavLink to="/"><img className={nav.sztoriLogo} src={pizzasztori}></img></NavLink>
+            </div>
+            <div className={nav.right} >
+                <NavLink to="/etlap"><div>Étlap</div></NavLink>
+                <NavLink to="/itallap"><div>Itallap</div></NavLink>
+                <NavLink to="/galeria"><div>Galéria</div></NavLink>
+                <NavLink to="kapcsolat"><div>Kapcsolat</div></NavLink>
+                <NavLink to="cikkek"><div>Cikkek</div></NavLink>
+            </div>
+        </div>
+        <Routes>
+            <Route path="/etlap" element={<Food />}/>
+        </Routes>
+        </BrowserRouter>
+        </>
+    )
+}
